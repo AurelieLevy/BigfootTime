d669fa61901a1f0116175f5f4e53397b4c5ebf17
root <root@REMI-SURFACE.localdomain>
2017-11-30 19:16:22 +0100

    Adding description and date

--- docs/assets/js/bfuMap.js
+++ docs/assets/js/bfuMap.js
@@ -1,13 +1,13 @@
-var svg = d3.select("#bfuMap");
-var projection = d3.geoAlbersUsa();
-projection.scale(1270,1270);
+let svg = d3.select("#bfuMap");
+let projection = d3.geoAlbersUsa();
+projection.scale(1270, 1270);
 projection.translate([480, 300]);
-console.log(projection.center);
 
-var path = d3.geoPath();
+let path = d3.geoPath();
+let dateFormatting = d3.timeFormat("%B %d, %Y");
 
 // The file loaded here is already projected with a d3.GeoAlbersUSA Projection
-d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function (error, us) {
+d3.json("data/us.json", function (error, us) {
 
     svg.append("path")
         .attr("stroke", "#303635")
@@ -41,14 +41,35 @@ d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function (error, us) {
         bf.features = bf.features
             .filter((f) => typeof (f.geometry.coordinates) != 'undefined' && f.geometry.coordinates != null);
 
-
+        // Drawing circle
         svg.selectAll("circle")
             .data(bf.features).enter()
             .append("circle")
             .attr("cx", function (d) { return d.geometry.coordinates[0]; })
             .attr("cy", function (d) { return d.geometry.coordinates[1]; })
-            .attr("r", "1px")
-            .attr("stroke", "#AAA");
-
-    })
+            .attr("r", "2px")
+            .attr("stroke", "#AAA")
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+    });
 });
+
+function handleMouseOver(d, i) {
+    console.log(d);
+    // Use D3 to select element, change color and size
+    d3.select(this)
+        .attr("fill", "#AAA")
+        .attr("r", "10px");
+
+    // Setting the current information
+    d3.select("#informationPlace")
+        .text(dateFormatting(new Date(d.properties.timestamp)) + " - " + d.properties.state + ", " + d.properties.county);
+    d3.select("#informationProperties")
+        .text(d.properties.observed);
+}
+
+function handleMouseOut(d, i) {
+    d3.select(this)
+        .attr("fill", null)
+        .attr("r", "3px");
+}
